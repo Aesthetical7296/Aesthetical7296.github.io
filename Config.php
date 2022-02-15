bce836a55739759937214a84995582bf9cf0067c
Lynz <79261532+LynzChloe@users.noreply.github.com>
2022-02-15 15:27:50 +0800

    Update Config.php

--- Config.php
+++ Config.php
@@ -3,7 +3,7 @@
 define('USER_DOMAIN', 'LynzChloe.github.io');
 # Url to redirect to when solving. Below is just an example, I recommend
 # putting a LinkVertise on top of it, so that way you can earn some money from ADs.
-define('USER_FINISH', 'https://' . USER_DOMAIN . '/Generate.php?solve=1');
+define('http://adfoc.us/7374571', 'https://' . USER_DOMAIN . '/Generate.php?solve=1');
 
 # Database Configuration
 define('DB_HOST', 'sql102.epizy.com');
